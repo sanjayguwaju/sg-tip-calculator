81623e8bcd160e9c8cbeb752ebdc9a0eaf1b045f
sanjayguwaju <sanjayguwaju@gmail.com>
2023-05-26 22:29:57 +0545

    changes done to the redux

---
+++ src/containers/NewItemFormContainer.js
@@ -0,0 +1,15 @@
+import {connect} from 'react-redux';
+import NewItemForm from '../components/NewItemForm';
+import {addNewItem} from '../store/items/actions';
+import { bindActionCreators } from 'redux';
+
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        onSubmit: (name, price) => dispatch(addNewItem(name, price))
+    }, dispatch); 
+};
+
+export const NewItemFormContainer = connect(null, mapDispatchToProps)(NewItemForm);
+
+
